73618786819fbc6204c6f85912e3ddf07b83893a
swwnull <15878741718@163.com>
2022-09-08 03:22:07 +0800

    new branches

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,6 @@ export const jwt= {
     // token:'jwt-token',
     expire:300//秒，口令的有效时间
 };
-export const MapKey = '87947e1aa74f42b2912608f521867e61'
+export const MapKey = ''
 // 地图限定城市
 export const MapCityName = '广西'
